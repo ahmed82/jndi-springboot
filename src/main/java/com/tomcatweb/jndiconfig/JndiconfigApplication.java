8d05f9c21d956871e6fed306ec2b5e3a90397c14
1426391 <ahmed.al-salih@delhaize.com>
2020-01-27 10:34:40 -0500

    spring boot web app jndi example

---
+++ src/main/java/com/tomcatweb/jndiconfig/JndiconfigApplication.java
@@ -0,0 +1,13 @@
+package com.tomcatweb.jndiconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JndiconfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JndiconfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tomcatweb/jndiconfig/JndiconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomcatweb.jndiconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JndiconfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
